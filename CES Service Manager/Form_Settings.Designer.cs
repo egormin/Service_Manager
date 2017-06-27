e53698e6f7f5799075eef9f45dc9d3a88922d92a
egormin <neoman2014@gmail.com>
2017-06-28 00:42:25 +0300

    Xml settings added

--- CES Service Manager/Form_Settings.Designer.cs
+++ CES Service Manager/Form_Settings.Designer.cs
@@ -32,16 +32,22 @@
             this.textEdit_Settings_IP = new DevExpress.XtraEditors.TextEdit();
             this.labelControl_Settings_IP = new DevExpress.XtraEditors.LabelControl();
             this.labelControl_Settings_PubKey = new DevExpress.XtraEditors.LabelControl();
-            this.textEdit_Settings_PubKey = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit_Settings_pubKey = new DevExpress.XtraEditors.TextEdit();
             this.simpleButton_Settings_PubKey = new DevExpress.XtraEditors.SimpleButton();
             this.simpleButton_Settings_TestConnection = new DevExpress.XtraEditors.SimpleButton();
             this.pictureEdit_Settings_NOK = new DevExpress.XtraEditors.PictureEdit();
             this.pictureEdit_Settings_OK = new DevExpress.XtraEditors.PictureEdit();
-            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton_Settings_save = new DevExpress.XtraEditors.SimpleButton();
+            this.textEdit_Settings_port = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl_Settings_port = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl_Settings_user = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit_Settings_user = new DevExpress.XtraEditors.TextEdit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit_Settings_IP.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit_Settings_PubKey.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit_Settings_pubKey.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit_Settings_NOK.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit_Settings_OK.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit_Settings_port.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit_Settings_user.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // textEdit_Settings_IP
@@ -54,6 +60,7 @@
             // labelControl_Settings_IP
             // 
             this.labelControl_Settings_IP.Appearance.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
+            this.labelControl_Settings_IP.Appearance.ForeColor = System.Drawing.Color.White;
             this.labelControl_Settings_IP.Location = new System.Drawing.Point(13, 39);
             this.labelControl_Settings_IP.Name = "labelControl_Settings_IP";
             this.labelControl_Settings_IP.Size = new System.Drawing.Size(150, 13);
@@ -63,23 +70,24 @@
             // labelControl_Settings_PubKey
             // 
             this.labelControl_Settings_PubKey.Appearance.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
-            this.labelControl_Settings_PubKey.Location = new System.Drawing.Point(13, 107);
+            this.labelControl_Settings_PubKey.Appearance.ForeColor = System.Drawing.Color.White;
+            this.labelControl_Settings_PubKey.Location = new System.Drawing.Point(12, 136);
             this.labelControl_Settings_PubKey.Name = "labelControl_Settings_PubKey";
             this.labelControl_Settings_PubKey.Size = new System.Drawing.Size(57, 13);
             this.labelControl_Settings_PubKey.TabIndex = 2;
             this.labelControl_Settings_PubKey.Text = "Public Key";
             // 
-            // textEdit_Settings_PubKey
+            // textEdit_Settings_pubKey
             // 
-            this.textEdit_Settings_PubKey.Location = new System.Drawing.Point(12, 126);
-            this.textEdit_Settings_PubKey.Name = "textEdit_Settings_PubKey";
-            this.textEdit_Settings_PubKey.Size = new System.Drawing.Size(222, 20);
-            this.textEdit_Settings_PubKey.TabIndex = 3;
+            this.textEdit_Settings_pubKey.Location = new System.Drawing.Point(12, 155);
+            this.textEdit_Settings_pubKey.Name = "textEdit_Settings_pubKey";
+            this.textEdit_Settings_pubKey.Size = new System.Drawing.Size(222, 20);
+            this.textEdit_Settings_pubKey.TabIndex = 3;
             // 
             // simpleButton_Settings_PubKey
             // 
             this.simpleButton_Settings_PubKey.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton_Settings_PubKey.Image")));
-            this.simpleButton_Settings_PubKey.Location = new System.Drawing.Point(240, 110);
+            this.simpleButton_Settings_PubKey.Location = new System.Drawing.Point(240, 136);
             this.simpleButton_Settings_PubKey.Name = "simpleButton_Settings_PubKey";
             this.simpleButton_Settings_PubKey.Size = new System.Drawing.Size(38, 36);
             this.simpleButton_Settings_PubKey.TabIndex = 4;
@@ -123,26 +131,67 @@
             this.pictureEdit_Settings_OK.TabIndex = 7;
             this.pictureEdit_Settings_OK.Visible = false;
             // 
-            // simpleButton1
+            // simpleButton_Settings_save
             // 
-            this.simpleButton1.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.Image")));
-            this.simpleButton1.ImageLocation = DevExpress.XtraEditors.ImageLocation.MiddleCenter;
-            this.simpleButton1.Location = new System.Drawing.Point(269, 262);
-            this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(40, 40);
-            this.simpleButton1.TabIndex = 8;
+            this.simpleButton_Settings_save.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton_Settings_save.Image")));
+            this.simpleButton_Settings_save.ImageLocation = DevExpress.XtraEditors.ImageLocation.MiddleCenter;
+            this.simpleButton_Settings_save.Location = new System.Drawing.Point(269, 262);
+            this.simpleButton_Settings_save.Name = "simpleButton_Settings_save";
+            this.simpleButton_Settings_save.Size = new System.Drawing.Size(40, 40);
+            this.simpleButton_Settings_save.TabIndex = 8;
+            this.simpleButton_Settings_save.Click += new System.EventHandler(this.simpleButton_Settings_save_Click);
+            // 
+            // textEdit_Settings_port
+            // 
+            this.textEdit_Settings_port.Location = new System.Drawing.Point(221, 59);
+            this.textEdit_Settings_port.Name = "textEdit_Settings_port";
+            this.textEdit_Settings_port.Size = new System.Drawing.Size(57, 20);
+            this.textEdit_Settings_port.TabIndex = 9;
+            // 
+            // labelControl_Settings_port
+            // 
+            this.labelControl_Settings_port.Appearance.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
+            this.labelControl_Settings_port.Appearance.ForeColor = System.Drawing.Color.White;
+            this.labelControl_Settings_port.Location = new System.Drawing.Point(221, 40);
+            this.labelControl_Settings_port.Name = "labelControl_Settings_port";
+            this.labelControl_Settings_port.Size = new System.Drawing.Size(24, 13);
+            this.labelControl_Settings_port.TabIndex = 10;
+            this.labelControl_Settings_port.Text = "Port";
+            // 
+            // labelControl_Settings_user
+            // 
+            this.labelControl_Settings_user.Appearance.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
+            this.labelControl_Settings_user.Appearance.ForeColor = System.Drawing.Color.White;
+            this.labelControl_Settings_user.Location = new System.Drawing.Point(13, 91);
+            this.labelControl_Settings_user.Name = "labelControl_Settings_user";
+            this.labelControl_Settings_user.Size = new System.Drawing.Size(26, 13);
+            this.labelControl_Settings_user.TabIndex = 12;
+            this.labelControl_Settings_user.Text = "User";
+            // 
+            // textEdit_Settings_user
+            // 
+            this.textEdit_Settings_user.Location = new System.Drawing.Point(13, 110);
+            this.textEdit_Settings_user.Name = "textEdit_Settings_user";
+            this.textEdit_Settings_user.Size = new System.Drawing.Size(91, 20);
+            this.textEdit_Settings_user.TabIndex = 11;
             // 
             // Form_Settings
             // 
+            this.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Appearance.Options.UseBackColor = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(321, 314);
-            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.labelControl_Settings_user);
+            this.Controls.Add(this.textEdit_Settings_user);
+            this.Controls.Add(this.labelControl_Settings_port);
+            this.Controls.Add(this.textEdit_Settings_port);
+            this.Controls.Add(this.simpleButton_Settings_save);
             this.Controls.Add(this.pictureEdit_Settings_OK);
             this.Controls.Add(this.pictureEdit_Settings_NOK);
             this.Controls.Add(this.simpleButton_Settings_TestConnection);
             this.Controls.Add(this.simpleButton_Settings_PubKey);
-            this.Controls.Add(this.textEdit_Settings_PubKey);
+            this.Controls.Add(this.textEdit_Settings_pubKey);
             this.Controls.Add(this.labelControl_Settings_PubKey);
             this.Controls.Add(this.labelControl_Settings_IP);
             this.Controls.Add(this.textEdit_Settings_IP);
@@ -150,24 +199,29 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form_Settings";
             ((System.ComponentModel.ISupportInitialize)(this.textEdit_Settings_IP.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit_Settings_PubKey.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit_Settings_pubKey.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit_Settings_NOK.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit_Settings_OK.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit_Settings_port.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit_Settings_user.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private DevExpress.XtraEditors.TextEdit textEdit_Settings_IP;
         private DevExpress.XtraEditors.LabelControl labelControl_Settings_IP;
         private DevExpress.XtraEditors.LabelControl labelControl_Settings_PubKey;
-        private DevExpress.XtraEditors.TextEdit textEdit_Settings_PubKey;
         private DevExpress.XtraEditors.SimpleButton simpleButton_Settings_PubKey;
         private DevExpress.XtraEditors.SimpleButton simpleButton_Settings_TestConnection;
         private DevExpress.XtraEditors.PictureEdit pictureEdit_Settings_NOK;
         private DevExpress.XtraEditors.PictureEdit pictureEdit_Settings_OK;
-        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton_Settings_save;
+        private DevExpress.XtraEditors.LabelControl labelControl_Settings_port;
+        public DevExpress.XtraEditors.TextEdit textEdit_Settings_IP;
+        public DevExpress.XtraEditors.TextEdit textEdit_Settings_pubKey;
+        public DevExpress.XtraEditors.TextEdit textEdit_Settings_port;
+        private DevExpress.XtraEditors.LabelControl labelControl_Settings_user;
+        public DevExpress.XtraEditors.TextEdit textEdit_Settings_user;
     }
 }
--- CES Service Manager/Set_TestConnection.cs
+++ CES Service Manager/Set_TestConnection.cs
@@ -9,14 +9,27 @@ namespace CES_Service_Manager
 {
     class Set_TestConnection
     {
-        public static string Check_Connection(string ip, string key)
+        public static string Check_Connection(string ip, string port, string user, string key)
         {
 
+            ConnectionInfo ConnInfo = new ConnectionInfo(ip, user, new AuthenticationMethod[]{
+                new PrivateKeyAuthenticationMethod(user, new PrivateKeyFile[]{ new PrivateKeyFile(@"" + key + "","") }),});
 
-
-
+            using (var sshclient = new SshClient(ConnInfo))
+            {
+                sshclient.Connect();
+                using (var cmd = sshclient.CreateCommand("mkdir /tmp/XXX"))
+                {
+                    cmd.Execute();
+                    //string sss = cmd.ExitStatus;
+                   // Console.WriteLine("Command>" + cmd.CommandText);
+                   // Console.WriteLine("Return Value = {0}", cmd.ExitStatus);
+                }
+                sshclient.Disconnect();
+            }
 
             return "OK";
         }
     }
 }
+// Key Based Authentication (using keys in OpenSSH Format)

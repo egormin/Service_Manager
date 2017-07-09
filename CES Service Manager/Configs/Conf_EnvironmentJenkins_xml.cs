581e7001fb7f2e6794ab1cf03a100f959946b530
egormin <neoman2014@gmail.com>
2017-07-09 17:20:55 +0300

    Jenkins environment configudation added

---
+++ CES Service Manager/Configs/Conf_EnvironmentJenkins_xml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+using System.Xml;
+
+namespace CES_Service_Manager
+{
+    class Conf_EnvironmentJenkins_xml
+    {
+        private string filePath = "config/Environment/Jenkins.xml";
+
+        public string LoadDataFromFile()
+        {
+            string result = "";
+
+            if (!Directory.Exists(filePath.Split('/')[1]))
+            {
+                Directory.CreateDirectory("config/" + filePath.Split('/')[1]);
+            }
+
+            if (File.Exists(filePath))
+            {
+                XDocument xdoc = XDocument.Load(filePath);
+                foreach (XElement el in xdoc.Root.Elements())
+                {
+                    result += el.Name + "#";
+                    //switch (Convert.ToString(el.Name))
+                    //{
+                    //    case "Jenkins":
+                    //        {
+                    //            result += el.Value;
+                    //            break;
+                    //        }
+                    //}
+                }
+            }
+            else
+            {
+                XmlTextWriter textWritter = new XmlTextWriter(filePath, Encoding.UTF8);
+
+                textWritter.WriteStartDocument();
+                textWritter.WriteStartElement("Jenkins");
+
+                textWritter.WriteEndElement();
+                textWritter.Close();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Change data in config file
+        /// </summary>
+        public void Change_settings(string path)
+        {
+
+            //XDocument doc = new XDocument(new XElement("JenkinsHosts",
+            // new XElement("Host", path)));
+
+            //   XDocument doc = new XDocument(new XElement("JenkinsHosts", new XElement("jenkins.epam.com", new XElement("httpLink", "https://jenkins/epam.com"), new XElement("host","evpbyminsv485"), new XElement("ip", "10.6.87.12"))));
+
+            //save config
+            //    doc.Save(filePath);
+            
+
+            XmlTextWriter textWritter = new XmlTextWriter(filePath, Encoding.UTF8);
+
+            textWritter.WriteStartDocument();
+            textWritter.WriteStartElement("Jenkins");
+
+            textWritter.WriteEndElement();
+            textWritter.Close();
+        }
+
+
+        public void Add_Server(string name, string http)
+        {
+            name = name.Replace(" ","");
+            XmlDocument document = new XmlDocument();
+            document.Load(filePath);    
+
+            XmlNode element = document.CreateElement(name);
+            document.DocumentElement.AppendChild(element);
+
+            XmlNode subElement = document.CreateElement("httpLink");
+            subElement.InnerText = http;
+            element.AppendChild(subElement);
+
+            document.Save(filePath);
+        }
+
+    }
+}
--- CES Service Manager/Form_EnvironmentJenkins.cs
+++ CES Service Manager/Form_EnvironmentJenkins.cs
@@ -15,14 +15,68 @@ namespace CES_Service_Manager
         public Form_EnvironmentJenkins()
         {
             InitializeComponent();
+            this.ClientSize = new Size(460, 160);
+
+            Conf_EnvironmentJenkins_xml inst_Conf_EnvironmentJenkins_xml = new Conf_EnvironmentJenkins_xml();
+            string[] settingsFromFile = inst_Conf_EnvironmentJenkins_xml.LoadDataFromFile().Split(new string[] { "#" }, StringSplitOptions.RemoveEmptyEntries);
+            //if settings were saved before, read these date and output to form
+            if (settingsFromFile.Length==0)
+            {
+                labelControl_Form_EnvironmentJenkins_NoServers.Visible = true;
+            } 
+            else
+            {
+                Change_label_NoServers(settingsFromFile);
+            }
+
+
+            textEdit_Form_EnvironmentJenkins_JenkinsName.Click += new EventHandler(textEdit_Form_EnvironmentJenkins_JenkinsName_Click);
+            textEdit_Form_EnvironmentJenkins_Link.Click += new EventHandler(textEdit_Form_EnvironmentJenkins_Link_Click);
         }
+
+
+        private void Change_label_NoServers(string [] settingsFromFile)
+        {
+            this.labelControl_Form_EnvironmentJenkins_ConfiguredServers.Visible = true;
+
+            labelControl_Form_EnvironmentJenkins_ServerList.Text = "";
+            labelControl_Form_EnvironmentJenkins_NoServers.Text = "Configured Servers: \r\n";
+
+            for (int i = 0; i < settingsFromFile.Length; i++)
+            {
+                labelControl_Form_EnvironmentJenkins_ServerList.Text += settingsFromFile[i] + "\r\n";
+            }
+            labelControl_Form_EnvironmentJenkins_ServerList.Visible = true;
+        }
+
+
+        Conf_EnvironmentJenkins_xml inst_Conf_EnvironmentJenkins_xml = new Conf_EnvironmentJenkins_xml();
+
 
         private void simpleButton_Form_EnvironmentJenkins_SaveSettings_Click(object sender, EventArgs e)
         {
-            Conf_EnvironmentJenkins_xml inst_Conf_EnvironmentJenkins_xml = new Conf_EnvironmentJenkins_xml();
+            inst_Conf_EnvironmentJenkins_xml.Add_Server(textEdit_Form_EnvironmentJenkins_JenkinsName.Text, textEdit_Form_EnvironmentJenkins_Link.Text);
+            Change_label_NoServers(inst_Conf_EnvironmentJenkins_xml.LoadDataFromFile().Split(new string[] { "#" }, StringSplitOptions.RemoveEmptyEntries));
+            MessageBox.Show("Server "+ textEdit_Form_EnvironmentJenkins_JenkinsName.Text + " has been added", "Saved successfully", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
 
-            inst_Conf_EnvironmentJenkins_xml.Change_settings(textEdit_Form_EnvironmentJenkins_Link.Text);
-            MessageBox.Show("Config has been saved", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        private void simpleButton_Form_EnvironmentJenkins_AddServer_Click(object sender, EventArgs e)
+        {
+            this.ClientSize = new Size(460, 310);
+            panel_Form_EnvironmentJenkins_AddServer.Visible = true;
+        }
+
+       
+        private void textEdit_Form_EnvironmentJenkins_JenkinsName_Click(object sender, EventArgs e)
+        {
+            textEdit_Form_EnvironmentJenkins_JenkinsName.Text = "";
+            this.textEdit_Form_EnvironmentJenkins_JenkinsName.Properties.Appearance.ForeColor = Color.Black;
+        }
+
+        private void textEdit_Form_EnvironmentJenkins_Link_Click(object sender, EventArgs e)
+        {
+            textEdit_Form_EnvironmentJenkins_Link.Text = "";
+            this.textEdit_Form_EnvironmentJenkins_Link.Properties.Appearance.ForeColor = Color.Black;
         }
     }
 }

70bd46cce7c0f08ebac80c4531bc2db21d2c73e6
egormin <neoman2014@gmail.com>
2017-07-10 23:39:20 +0300

    Open project link added

--- CES Service Manager/Forms/Form_Jenkins_CreateProject.Designer.cs
+++ CES Service Manager/Forms/Form_Jenkins_CreateProject.Designer.cs
@@ -28,8 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel();
             this.textEdit_Form_Jenkins_CreateProject_ProjectName = new DevExpress.XtraEditors.TextEdit();
             this.labelControl_Form_Jenkins_CreateProject_ProjectName = new DevExpress.XtraEditors.LabelControl();
             this.simpleButton_Form_Jenkins_CreateProject_CreateProject = new DevExpress.XtraEditors.SimpleButton();
@@ -37,6 +36,7 @@
             this.textBox_Form_Jenkins_CreateProject_Result = new System.Windows.Forms.TextBox();
             this.comboBoxEdit_Form_Jenkins_CreateProject_ChooseServer = new DevExpress.XtraEditors.ComboBoxEdit();
             this.labelControl_Form_Jenkins_CreateProject_ChooseJenkins = new DevExpress.XtraEditors.LabelControl();
+            this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject = new DevExpress.XtraEditors.HyperlinkLabelControl();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit_Form_Jenkins_CreateProject_ProjectName.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit_Form_Jenkins_CreateProject_ChooseServer.Properties)).BeginInit();
             this.SuspendLayout();
@@ -116,6 +116,20 @@
             this.labelControl_Form_Jenkins_CreateProject_ChooseJenkins.TabIndex = 11;
             this.labelControl_Form_Jenkins_CreateProject_ChooseJenkins.Text = "Choose Jenkins";
             // 
+            // hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject
+            // 
+            this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject.Appearance.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject.Appearance.ForeColor = System.Drawing.Color.Navy;
+            this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject.Location = new System.Drawing.Point(21, 237);
+            this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject.Name = "hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject";
+            this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject.Size = new System.Drawing.Size(75, 13);
+            this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject.TabIndex = 12;
+            this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject.Tag = "sfsfsf";
+            this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject.Text = "Open project...";
+            this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject.Visible = false;
+            this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject.Click += new System.EventHandler(this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject_Click);
+            // 
             // Form_Jenkins_CreateProject
             // 
             this.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
@@ -123,6 +137,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 273);
+            this.Controls.Add(this.hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject);
             this.Controls.Add(this.labelControl_Form_Jenkins_CreateProject_ChooseJenkins);
             this.Controls.Add(this.comboBoxEdit_Form_Jenkins_CreateProject_ChooseServer);
             this.Controls.Add(this.textBox_Form_Jenkins_CreateProject_Result);
@@ -150,5 +165,6 @@
         private System.Windows.Forms.TextBox textBox_Form_Jenkins_CreateProject_Result;
         private DevExpress.XtraEditors.ComboBoxEdit comboBoxEdit_Form_Jenkins_CreateProject_ChooseServer;
         private DevExpress.XtraEditors.LabelControl labelControl_Form_Jenkins_CreateProject_ChooseJenkins;
+        private DevExpress.XtraEditors.HyperlinkLabelControl hyperlinkLabelControl_Form_Jenkins_CreateProject_OpenProject;
     }
 }
